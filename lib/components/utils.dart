281273e668faa3d7bb596faf9f715528800f5294
Rajib Ahmed <rajibdpi@gmail.com>
2024-05-30 01:45:14 +0600

    added option for updated

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -17,14 +17,13 @@ Future<List<Map>> lastUpdatedLocalFile() async {
   final file = File('${directory.path}/$appDatabaseName');
   final localFileStat = await file.stat();
   localFileSize = localFileStat.size;
-  print("localFileSize:$localFileSize");
-
+  // print("localFileSize:$localFileSize");
 // lastUpdatedOnlineFile()
   const fileUrl =
       'https://raw.githubusercontent.com/rajibdpi/govdictionary/latest/assets/words.json';
   final response = await http.get(Uri.parse(fileUrl));
   onlinefileSize = response.bodyBytes.length;
-  print('OnlineFileSize:$onlinefileSize');
+  // print('OnlineFileSize:$onlinefileSize');
   DateTime localUpdatedDateTime = localFileStat.modified;
   String updatedAt = localUpdatedDateTime.toString();
   // return updatedAt;
@@ -35,18 +34,6 @@ Future<List<Map>> lastUpdatedLocalFile() async {
       'onlinefileSize': onlinefileSize
     }
   ];
-
-  // const owner = 'rajibdpi';
-  // const repo = 'govdictionary';
-  // const filePath = 'assets/words.json';
-  // 'https://raw.githubusercontent.com/rajibdpi/govdictionary/main/assets/words.json';
-  // 'https://cdn.jsdelivr.net/gh/rajibdpi/govdictionary@main/assets/words.json';
-  // const fileUrl ='https://api.github.com/repos/$owner/$repo/contents/$filePath';
-  // numberOfEntries = jsonData.length;
-  // print('numberOfEntries:$numberOfEntries');
-  // numberOfKeys = jsonData.fold(0, (sum, item) => sum + item.keys.length);
-  // onLineFileSize = (onlineData as Map)['size'].toString();
-  // print("OnlineFileS:$onLineFileSize");
 }
 
 // checkUpdate
@@ -54,16 +41,16 @@ bool updateAvailable() {
   return localFileSize < onlinefileSize ? true : false;
 }
 
+// saveUpdate
 Future<void> saveUpdate() async {
   final directory = await getApplicationDocumentsDirectory();
-
   final response = await http.get(
     Uri.parse(
         'https://raw.githubusercontent.com/rajibdpi/govdictionary/latest/assets/words.json'),
   );
   if (response.statusCode == 200) {
     final jsonString = response.body;
-    final List<dynamic> jsonData = jsonDecode(jsonString);
+    // final List<dynamic> jsonData = jsonDecode(jsonString);
     // setState(() {
     //   allWords = jsonData.map((wordJson) => Word.fromJson(wordJson)).toList();
     //   filteredWords = allWords;
