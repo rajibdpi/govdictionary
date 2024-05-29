4735068eb16635fd4839b0006448ad4beee8c6e8
Rajib Ahmed <rajibdpi@gmail.com>
2024-05-30 01:53:58 +0600

    Updated saveData

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -4,12 +4,13 @@ import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 
 //lastUpdateLocalFile()
-
 String appDatabaseName = 'words.json';
 int localFileSize = 0;
 int onlinefileSize = 0;
 int numberOfEntries = 0;
 int numberOfKeys = 0;
+String remoteFileUrl =
+    'https://raw.githubusercontent.com/rajibdpi/govdictionary/latest/assets/words.json';
 
 Future<List<Map>> lastUpdatedLocalFile() async {
   final directory = await getApplicationDocumentsDirectory();
@@ -18,9 +19,7 @@ Future<List<Map>> lastUpdatedLocalFile() async {
   localFileSize = localFileStat.size;
 
   // lastUpdatedOnlineFile()
-  const fileUrl =
-      'https://raw.githubusercontent.com/rajibdpi/govdictionary/latest/assets/words.json';
-  final response = await http.get(Uri.parse(fileUrl));
+  final response = await http.get(Uri.parse(remoteFileUrl));
   onlinefileSize = response.bodyBytes.length;
   DateTime localUpdatedDateTime = localFileStat.modified;
   String updatedAt = localUpdatedDateTime.toString();
@@ -42,21 +41,21 @@ bool updateAvailable() {
 Future<void> saveUpdate() async {
   final directory = await getApplicationDocumentsDirectory();
   final response = await http.get(
-    Uri.parse(
-        'https://raw.githubusercontent.com/rajibdpi/govdictionary/latest/assets/words.json'),
+    Uri.parse(remoteFileUrl),
   );
   if (response.statusCode == 200) {
     final jsonString = response.body;
-    // final List<dynamic> jsonData = jsonDecode(jsonString);
-    // setState(() {
-    //   allWords = jsonData.map((wordJson) => Word.fromJson(wordJson)).toList();
-    //   filteredWords = allWords;
-    //   isLoading = false;
-    // });
-    // Save JSON data locally with a custom file name
     await File('${directory.path}/words.json').writeAsString(jsonString);
     // print(jsonString);
   } else {
     throw Exception('Failed to load words');
   }
+
+  // final List<dynamic> jsonData = jsonDecode(jsonString);
+  // setState(() {
+  //   allWords = jsonData.map((wordJson) => Word.fromJson(wordJson)).toList();
+  //   filteredWords = allWords;
+  //   isLoading = false;
+  // });
+  // Save JSON data locally with a custom file name
 }
