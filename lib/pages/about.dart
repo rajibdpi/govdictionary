9bcbd7c36d6ea805342f1f83aa4f2800bf65d39a
Rajib Ahmed <rajibdpi@gmail.com>
2024-05-31 01:25:06 +0600

    Updatwed about page

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -18,39 +18,42 @@ class _AboutPageState extends State<AboutPage> {
         // actions: [],
         title: const Text(
           "About",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontSize: 15),
         ),
       ),
-      body: Center(
-        child: FutureBuilder<List<Map>>(
-          future: fileStats(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // While waiting for the future to complete, you can display a loading indicator or placeholder text
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              // If there's an error, you can display an error message
-              return Text('Error: ${snapshot.error}');
-            } else {
-              // If the future completes successfully, display the last update time
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton.icon(
-                    onPressed: updateAvailable() == true
-                        ? () {
-                            saveUpdate();
-                          }
-                        : null,
-                    label: updateAvailable() == true
-                        ? const Text('Update')
-                        : const Text('Already Updated'),
-                  ),
-                  Text('UpdatedAt: ${snapshot.data.toString()}')
-                ],
-              );
-            }
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: FutureBuilder<List<Map>>(
+            future: fileStats(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // While waiting for the future to complete, you can display a loading indicator or placeholder text
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                // If there's an error, you can display an error message
+                return Text('Error: ${snapshot.error}');
+              } else {
+                // If the future completes successfully, display the last update time
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton.icon(
+                      onPressed: updateAvailable() == true
+                          ? () {
+                              saveUpdate();
+                            }
+                          : null,
+                      label: updateAvailable() == true
+                          ? const Text('Update')
+                          : const Text('Already Updated'),
+                    ),
+                    Text('UpdatedAt: ${snapshot.data.toString()}')
+                  ],
+                );
+              }
+            },
+          ),
         ),
       ),
     );
